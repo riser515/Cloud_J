3337d08c213bc723ab6a98906df4c6fa3b40bea5
Khushi <sparker.star001@gmail.com>
2021-07-11 01:56:15 +0530

    Init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) => 
+item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+    })
+);
